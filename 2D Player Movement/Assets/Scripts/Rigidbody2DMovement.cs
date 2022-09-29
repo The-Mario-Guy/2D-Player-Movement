a3a329c0a91bfaa2039c8b5a28822c1ba6bcb28b
The-Mario-Guy <111007100+The-Mario-Guy@users.noreply.github.com>
2022-09-29 11:05:22 -0700

    Added Jump

---
+++ 2D Player Movement/Assets/Scripts/Rigidbody2DMovement.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rigidbody2DMovement : MonoBehaviour
+{
+    
+     public float moveSpeed = 5;
+    public float jumpForce = 10;
+
+    public bool isOnGround;
+    private Rigidbody2D _myRB;
+    private Collider2D _myCollider;
+   // Start is called before the first frame update 
+   void Start()
+    {
+   _myRB = GetComponent<Rigidbody2D>();
+   _myCollider = GetComponent<Collider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        _myRB.velocity = new Vector2(horizontalInput * moveSpeed, _myRB.velocity.y);
+
+        if(Input.GetKeyDown(KeyCode.Z))
+        {
+            _myRB.velocity = new Vector2(_myRB.velocity.x, jumpForce);
+        }
+    }
+}
